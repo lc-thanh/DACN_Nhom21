0aa36240c0651225ebbaadace78f454fe058cbc4
Le Cong Thanh <lcthanh.htvn@gmail.com>
2024-12-25 23:59:54 +0700

    Delete API/LibraryManagerApp/Database1 directory

--- API/LibraryManagerApp/Database1/dbo/Tables/Cabinets.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Cabinets] (
-    [Id]        UNIQUEIDENTIFIER NOT NULL,
-    [Name]      NVARCHAR (50)    NOT NULL,
-    [Location]  NVARCHAR (50)    NULL,
-    [CreatedOn] DATETIME2 (7)    NOT NULL,
-    CONSTRAINT [PK_Cabinets] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
--- API/LibraryManagerApp/Database1/dbo/Tables/Categories.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories] (
-    [Id]          UNIQUEIDENTIFIER NOT NULL,
-    [Name]        NVARCHAR (50)    NOT NULL,
-    [Description] NVARCHAR (2000)  NULL,
-    [CreatedOn]   DATETIME2 (7)    NOT NULL,
-    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
